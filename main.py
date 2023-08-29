454f77caa07101f039e3091cead064de494704c3
malenovanata <137698439+malenovanata@users.noreply.github.com>
2023-08-29 22:03:56 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+
+import json
+import random
+import time
+from web3 import Web3
+
+from fake_useragent import UserAgent
+from loguru import logger
+from eth_account.messages import encode_defunct
+import asyncio
+import aiohttp
+from web3.eth import AsyncEth
